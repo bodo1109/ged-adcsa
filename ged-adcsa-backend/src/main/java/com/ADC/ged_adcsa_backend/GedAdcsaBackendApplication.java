68596c4c57b3ea1936cdd9c6002a1a139bee18d8
Plnbbd <babodopaulin@gmail.com>
2025-06-06 15:45:41 +0200

    first commit

---
+++ ged-adcsa-backend/src/main/java/com/ADC/ged_adcsa_backend/GedAdcsaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ADC.ged_adcsa_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GedAdcsaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GedAdcsaBackendApplication.class, args);
+	}
+
+}
---
+++ ged-adcsa-backend/src/test/java/com/ADC/ged_adcsa_backend/GedAdcsaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ADC.ged_adcsa_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GedAdcsaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
